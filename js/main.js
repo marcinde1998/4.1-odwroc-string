f6832ccc62157cb51b8bd87bd4ccf3eb0cf04b08
marcinde1998 <marcinde1998@gmail.com>
2022-03-13 19:50:00 +0100

    jeden

---
+++ js/main.js
@@ -0,0 +1,7 @@
+
+
+const reverseStringReverse = function(textToReverse){
+    return textToReverse.split("").reverse().join("");
+  };
+  let wynik = reverseStringReverse("Akademia108");
+  console.log(wynik);
